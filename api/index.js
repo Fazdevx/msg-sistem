902d66db2b1d91dbe88b10a5e75eb7d2b2afa943
Fazdevx <fabrizioberrospi21@gmail.com>
2026-08-26 15:39:25 -0500

    Load server/.env in Vercel API handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
+import { config } from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+config({ path: join(__dirname, "..", "server", ".env") });
+
 import { app } from "../server/src/app.js";
 import serverless from "serverless-http";
 
